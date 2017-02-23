3752f955b94d93f6c3a83ecd863193193ca77cbf
omarkhatib <omarkh1995@yahoo.com>
2017-02-23 20:59:24 +0200

    angular filters

---
+++ Angular/filters/F_C/js.js
@@ -0,0 +1,14 @@
+var app = angular.module('app',[]);
+app.filter('temp', function() {
+    return function(x) {
+        var i, c, txt = "";
+        for (i = 0; i < x.length; i++) {
+            c = x[i];
+            if (i % 2 == 0) {
+                c = c.toUpperCase();
+            }
+            txt += c;
+        }
+        return txt;
+    };
+});
