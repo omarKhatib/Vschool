e5571629eb6dda0b0174596efb965f37ef5d7d77
omarkhatib <omarkh1995@yahoo.com>
2017-03-02 19:14:33 +0200

    DELETE request

--- nodeJs/express/requests/app.js
+++ nodeJs/express/requests/app.js
@@ -9,17 +9,17 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
-app.get('/',function(req,res){ //default : root dir
+app.get('/',function(req,res){                //default : root dir
     res.status(200).send(data);
 });
 
 app.post('/',function(req,res){
-    if(req.body.name ==undefined || req.body.name ==''){      //name validation
+    if(req.body.name ==undefined || req.body.name ==''){      //name validation , If the user enters extra enteties : will neglect it automatically
         res.status(400).send({'message':'you have to insert name'});
     }
 
     else{
-    data.push({                        //req.body : the entire posted json object
+    data.push({                        //req.body : the entire posted json object , only in post use body
         id: uuid.v4(),
         name: req.body.name,
         age: req.body.age
@@ -29,6 +29,33 @@ app.post('/',function(req,res){
     }
     
 });
+
+
+
+app.delete('/:id', function(req, res){
+    var id = req.params.id;
+    var found = false;
+    for(var i = 0; i<data.length ; i++){
+        if(data[i].id == id){
+            data.splice(i, 1);
+            res.status(200).send({'message': 'deleted!'});
+            found =true
+        }
+  
+    }
+    if(found == false) {
+         res.status(400).send({'message': 'id not found!'});
+        
+    }
+     
+    
+    
+});
+
+
+
+
+
 
 var server = app.listen(post,function(){
     console.log("We have started our server on port : "+post);
