0f6cb1fd1acd9c56cb7bcf80377785cde92b4276
omarkhatib <omarkh1995@yahoo.com>
2017-03-07 21:00:39 +0200

    get data from db

---
+++ mongo/CRED/app.js
@@ -0,0 +1,25 @@
+var mongodb = require('mongodb');
+
+//setup
+
+var client = mongodb.MongoClient;
+var URL = 'mongodb://localhost:27017/students';
+client.connect(URL, function(err, db){
+    if(err){
+        console.log(err);
+        
+    }
+    
+    else{
+        var collection = db.collection('coll');
+        collection.insert({name:'ahmadDada', age:1}, function(err, result){
+            collection.find().toArray(function(err, docs){
+                console.log(docs);
+                db.close();
+            });
+            
+        });
+        
+    }
+    
+});
