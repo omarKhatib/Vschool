4acada3e9d396da7ebd7b6712e628ab5d635d879
omarkhatib <omarkh1995@yahoo.com>
2017-03-10 20:12:27 +0200

    mongoose-Express-CRUD-no view

---
+++ mongo/students-express-CRUD-noview/server/api.js
@@ -0,0 +1,181 @@
+var express = require("express");
+var mongoose = require('mongoose');
+var data = require("./students.js");
+var bodyParser = require('body-Parser');
+var apiRouter = express.Router();
+
+
+apiRouter.use(bodyParser.urlencoded({
+    extended: false
+}));
+apiRouter.use(bodyParser.json());
+
+
+apiRouter.get("/students", function (req, res) {
+ data.find({}, function(err, data){
+     if(err){
+         res.status(500).send({message:'internal server error'});
+         
+     }
+     else{
+         
+         res.status(200).send({data: data});
+         
+     }
+     
+     
+     
+     
+     
+ })
+
+});
+
+apiRouter.post("/students",function(req, res){
+    var newData = new data(req.body);
+    newData.save(function(err, nd){
+        if(err){
+            res.status(500).send({meassage:'error'});
+            
+        }
+        
+        else{
+            res.status(200).send({data:nd});
+            
+        }
+        
+    })
+    
+});
+
+
+
+
+apiRouter.delete("/students/:id", function(req, res){
+   // console.log(req.params.id);
+    data.findOne({_id:req.params.id}, function(err, data){
+        if(err){
+            res.status(500).send({message:'internal error'+ err});
+            
+        }
+        else if(data == undefined){
+            
+            res.status(404).send({message:'not found'});
+        }
+        
+        else{
+            data.remove(function(err, data){
+                if(err){
+                    res.status(500).send({message:'internal error'+ err});
+                    
+                }
+                
+                else{
+                    
+                    res.status(200).send({message:'removed!'});
+                }
+                
+                
+            });
+            
+        }
+        
+    });
+    
+    
+});    //dont need to save after remove
+
+
+
+apiRouter.put("/students/:id", function(req, res){
+   // console.log(req.params.id);
+    data.findOne({_id:req.params.id}, function(err, data){
+        if(err){
+            res.status(500).send({message:'internal error'+ err});
+            
+        }
+        else if(data == undefined){
+            
+            res.status(404).send({message:'not found'});
+        }
+        
+        else{
+            for(key in req.query){
+                data[key] = req.query[key]
+                
+            }
+            data.save();
+            res.status(200).send({updatedData:data })
+          
+            
+        }
+        
+    });
+    
+    
+});    //dont need to save after remove
+
+
+
+
+
+//apiRouter.get("/students/:id", function (req, res) {
+//    var id = req.params.id;
+//    for (var i = 0; i < data.length; i++) {
+//        if (data[i].id == id) {
+//            res.status(200).send(data[i]);
+//        }
+//    }
+//});
+//
+//
+//apiRouter.post("/students", function (req, res) {
+//    var dataList = {
+//        id: uuid.v4(),
+//        firstName: req.body.firstName,
+//        lastName: req.body.lastName,
+//        living: req.body.living,
+//        bountyAmount: req.body.bountyAmount,
+//        type: req.body.type
+//    };
+//    var didpass = fnValidate(dataList);
+//    if (didpass.pass == false) {
+//        res.status(400).send({
+//            message: didpass.message
+//        })
+//    } else {
+//        data.push(dataList);
+//        res.status(200).send({
+//            sucess: true
+//        });
+//    }
+//});
+//
+//apiRouter.delete("/students/:id", function (req, res) {
+//    var id = req.params.id;
+//    for (var i = 0; i < data.length; i++) {
+//        if (data[i].id == id) {
+//            data.splice(i, 1);
+//            res.status(200).send({
+//                sucess: true
+//            });
+//        }
+//    }
+//
+//});
+//
+//
+//apiRouter.put("/students/:id", function (req, res) {
+//    var id = req.params.id;
+//    for (var i = 0; i < data.length; i++) {
+//        if (data[i].id == id) {
+//            for (key in req.query) {
+//                data[i][key] = req.query[key];
+//            }
+//            res.status(200).send(data[i]);
+//        }
+//    }
+//
+//});
+
+module.exports=apiRouter;
---
+++ mongo/students-express-CRUD-noview/server/server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/');
+
+var apiRouter=require("./api.js");
+var PORT = process.env.PORT || 8080;
+
+
+
+
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
+app.use(apiRouter);
+
+
+app.listen(PORT, function () {
+    console.log("iam listing on " + PORT);
+})
