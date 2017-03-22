2e67418227f4183682e6015777b502a8b0777ad7
omarkhatib <omarkh1995@yahoo.com>
2017-03-22 21:05:03 +0200

    mongo auth

---
+++ mongo/secur/auth/server/auth.js
@@ -0,0 +1,5 @@
+var express = require('expess');
+var authRouter = express.Router();
+var mongoose = require('mongoose');
+var bodyParser = require('body-Parser');
+var Users = require('./users.js');
