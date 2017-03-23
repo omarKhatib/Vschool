3bc70cf863e8b3d23711dac8213504c7e7fafd5c
omarkhatib <omarkh1995@yahoo.com>
2017-03-23 19:48:45 +0200

    mongo express auth , username and password checking

--- mongo/secur/auth/server/auth.js
+++ mongo/secur/auth/server/auth.js
@@ -1,5 +1,42 @@
-var express = require('expess');
+var express = require('express');
 var authRouter = express.Router();
 var mongoose = require('mongoose');
 var bodyParser = require('body-Parser');
-var Users = require('./users.js');
+var User = require('./users.js');
+
+
+authRouter.post("/signup",function(req,res){
+    User.find({username:req.body.username},function(err,data){
+        if(err){
+            res.status(500).send(err);
+        }else if (data.length>0){
+            res.status(200).send({"message":"username is taken"});
+        }else {
+            var newUser =new User(req.body);
+            newUser.save(function(err,data){
+                if(err){
+                    res.status(500).send(err);
+                }else {
+                    res.status(200).send({"message":"you just signup "});
+                }
+            })
+        }
+    });
+});
+
+authRouter.post("/signin",function(req,res){
+     User.findOne({username:req.body.username},function(err,data){
+         if(err){
+             res.status(500).send(err);
+         }else if (data == undefined){
+            res.status(400).send({"message":"username is wrong"});
+         }else if(data.password != req.body.password){
+              res.status(400).send({"message":"password is worng"});
+         }else {
+             res.status(200).send({"message":"you just signin"});
+         }
+     })
+})
+
+
+module.exports =authRouter;
